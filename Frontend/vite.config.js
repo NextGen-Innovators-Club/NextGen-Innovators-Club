eb5ce6d86f234579f2fefd0a08f58a2d98f801e3
Ayush Singh <ayushsingh355vns@gmail.com>
2025-10-03 21:12:34 +0530

    Commit

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,12 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/',
-})
-
-
+export default {
+  base: '/', // remove subfolder reference
+}
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/',
+// })
 
 
